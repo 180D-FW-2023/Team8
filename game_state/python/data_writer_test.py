64e5e598e4b6ad994a32bff2efcd7c3c5b8ea26b
Nabeel Shahid <nshahid@ucla.edu>
2023-11-20 17:24:14 -0800

    update paths with os.path.join

--- game_state/python/data_writer_test.py
+++ game_state/python/data_writer_test.py
@@ -3,8 +3,11 @@ import numpy as np
 import os
 import time
 
-file_path = "../../input_output/sensor_data.csv"
+file_path = os.path.join("..", "..", "input_output", "sensor_data.csv")
+dir_path = os.path.dirname(__file__)
+os.chdir(dir_path)
 full_path = os.path.abspath(os.path.join(os.getcwd(), file_path))
+
 i = 0
 while True:
     with open(full_path, 'w') as file:
