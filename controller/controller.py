8efc9edc2498bf2dab8dbd3398bfa07638582786
Nabeel Shahid <nshahid@ucla.edu>
2023-11-28 10:35:23 -0800

    add threads and subprocesses to controller

--- controller/controller.py
+++ controller/controller.py
@@ -3,15 +3,20 @@ import subprocess
 import time
 import os
 
-#data_writer_test_path = os.path.join("..", "game_state", "python", "data_writer_test.py")
-update_state_path = os.path.join("..", "game_state", "python", "update_state.py")
-striker_tracking_path = os.path.join("..", "camera", "Green_Striker_Tracking.py")
+def run_subprocesses():
+    #data_writer_test_path = os.path.join("..", "game_state", "python", "data_writer_test.py")
+    update_state_path = os.path.join("..", "game_state", "python", "update_state.py")
+    striker_tracking_path = os.path.join("..", "camera", "Green_Striker_Tracking.py")
 
-#data_writer_test = subprocess.Popen(['python', data_writer_test_path])
-striker_tracking = subprocess.Popen(['python', striker_tracking_path])
-update_state = subprocess.Popen(['python', update_state_path])
+    #data_writer_test = subprocess.Popen(['python', data_writer_test_path])
+    striker_tracking = subprocess.Popen(['python', striker_tracking_path])
+    update_state = subprocess.Popen(['python', update_state_path])
 
-time.sleep(15)
-#data_writer_test.kill()
-update_state.kill()
-striker_tracking.kill()
+    time.sleep(15)
+    #data_writer_test.kill()
+    update_state.kill()
+    striker_tracking.kill()
+    return
+
+def run_threads():
+    pass
