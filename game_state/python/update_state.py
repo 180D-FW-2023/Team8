d68cecc7683dce91aa7b63ae23f1de8f45d063a8
Nabeel Shahid <nshahid@ucla.edu>
2024-01-23 18:54:36 -0800

    added 2D movement and physics

--- game_state/python/update_state.py
+++ game_state/python/update_state.py
@@ -17,8 +17,8 @@ class GameState:
         # Setup figure and axes
         self.fig, self.ax = plt.subplots()
         self.ax.set_aspect("equal")
-        self.ax.set_xlim(0,self.x_max)
-        self.ax.set_ylim(0,self.y_max)
+        self.ax.set_xlim(0,self.y_max)
+        self.ax.set_ylim(0,self.x_max)
         self.ax.get_xaxis().set_ticks([])
         self.ax.get_yaxis().set_ticks([])
         self.ax.get_xaxis().set_visible(False)
@@ -33,8 +33,8 @@ class GameState:
         self.font = pygame.font.Font(None, 36)
 
         # Draw Scene
-        center_line = np.array([[self.x_max/2, self.x_max/2], [0, self.y_max]])
-        center_circle = patches.Circle((self.x_max/2, self.y_max/2), radius = self.x_max/10, edgecolor = 'gray', fill = False, linewidth = 1, zorder = 1)
+        center_line = np.array([[0, self.y_max], [self.x_max/2, self.x_max/2]])
+        center_circle = patches.Circle((self.y_max/2, self.x_max/2), radius = self.y_max/10, edgecolor = 'gray', fill = False, linewidth = 1, zorder = 1)
         self.ax.add_patch(center_circle)
         self.ax.plot(center_line[0, :], center_line[1, :], color = 'gray', linewidth = 1, zorder = 1)
 
@@ -96,6 +96,7 @@ class Striker(Actor):
         self.y_dim = self.game_state.y_max/5
         self.inertia = inertia
         edge_offset = 1/20
+        self.max_velocity = self.y_max/100
        
         # Create plot
         if is_left_striker:
@@ -112,35 +113,46 @@ class Striker(Actor):
     def move(self, loc):
         inertia = self.inertia*0
         previous_pos = self.position[1]
-        self.position[1] = (inertia)*self.position[1] + (1-inertia)*(loc+1)/2*(self.y_max-self.y_dim)
-        self.velocity = self.position[1] - previous_pos
+        next_pos = (inertia)*self.position[1] + (1-inertia)*(loc+1)/2*(self.y_max-self.y_dim)
+        self.velocity = next_pos - previous_pos
+        if np.abs(self.velocity) < self.max_velocity:
+            self.position[1] = next_pos
+        else:
+            self.position[1] += np.sign(self.velocity)*self.max_velocity
+
         self.verticies = np.column_stack((self.position + np.array([0,0]), self.position + np.array([0,self.y_dim]), self.position + np.array([self.x_dim,self.y_dim]), self.position + np.array([self.x_dim,0]), self.position + np.array([0,0])))
         
     def draw(self):
-        self.plot.set_data(self.verticies)
+        self.plot.set_data(self.verticies[[1,0]])
         pygame_points = (self.verticies[:, 0], self.verticies[:, 1], self.verticies[:, 2], self.verticies[:, 3])  
         pygame.draw.polygon(self.game_state.screen, "green", pygame_points)
 
 class StrikerCPU(Striker):
     def move(self, loc):
-        max_velocity = 1/100*self.game_state.y_max
-        ball_loc = self.game_state.ball.position[1] - self.y_dim/2
-        previous_pos = self.position[1]
-        inertia = self.inertia
+        max_velocity = self.max_velocity*1000
+        ball_y_loc = self.game_state.ball.position[1] - self.y_dim/2
+        ball_x_loc = self.game_state.ball.position[0]
+        x_boundary = (4/5)*self.game_state.x_max
+        previous_y_pos = self.position[1]
+        previous_x_pos = self.position[0]
+        inertia = self.inertia*0
         
-        next_position = (inertia)*self.position[1] + (1-inertia)*(ball_loc)
-        
-        is_in_bounds = (next_position + self.y_dim < self.game_state.y_max) and (next_position > 0)
+        next_y_position = (inertia)*previous_y_pos + (1-inertia)*(ball_y_loc)
 
-        self.velocity = next_position - previous_pos
+        normalized_distance_to_boundary = np.min(((x_boundary-ball_x_loc)/x_boundary, 1))
+        next_x_position = (inertia)*previous_x_pos + (1-inertia)*(x_boundary + (self.game_state.x_max-x_boundary-self.x_dim)*normalized_distance_to_boundary)
+        
+        is_in_bounds = (next_y_position + self.y_dim < self.game_state.y_max) and (next_y_position > 0)
+
+        self.velocity = np.array([next_x_position - previous_x_pos, next_y_position - previous_y_pos])
         
         if is_in_bounds: 
-            if np.abs(self.velocity) < max_velocity:    
-                self.position[1] = next_position
-            if self.velocity > max_velocity:
-                self.position[1] += max_velocity
-            if self.velocity < -max_velocity:
-                self.position[1] += -max_velocity
+            if np.abs(self.velocity[1]) < max_velocity:    
+                self.position[1] = next_y_position
+            else:
+                self.position[1] += np.sign(self.velocity[1])*max_velocity
+        
+        self.position[0] = next_x_position
         
         self.verticies = np.column_stack((self.position + np.array([0,0]), self.position + np.array([0,self.y_dim]), self.position + np.array([self.x_dim,self.y_dim]), self.position + np.array([self.x_dim,0]), self.position + np.array([0,0])))
 
@@ -150,7 +162,8 @@ class Ball(Actor):
         Actor.__init__(self, game_state)
         self.v_mag = self.game_state.v_mag        
         self.position_history = np.zeros((2, 5))
-        self.velocity = self.game_state.v_mag*np.array([-np.sqrt(2)/2, -np.sqrt(2)/2])
+        self.inital_velocity = self.game_state.v_mag*np.array([-np.sqrt(2)/2, -np.sqrt(2)/2])
+        self.velocity = self.inital_velocity.copy()
         self.plot = self.game_state.ax.scatter([], [], color = 'red')
         self.position = np.array([self.game_state.x_max/2, self.game_state.y_max/2])
 
@@ -166,19 +179,25 @@ class Ball(Actor):
         hit_bottom_edge = next_position[1] <= 0
 
         if hit_left_edge:
-            self.velocity[0] = -1*self.velocity[0]
+            self.velocity = self.inital_velocity.copy()
             self.game_state.score_point(is_left_point = False)
+            return
         elif hit_right_edge:
-            self.velocity[0] = -1*self.velocity[0]
+            self.velocity = -1*self.inital_velocity.copy()
             self.game_state.score_point(is_left_point = True)
+            return
 
         if hit_top_edge or hit_bottom_edge:
             self.velocity[1] = -1*self.velocity[1]
         
         if (left_striker.position[1] < self.position[1] < left_striker.position[1] + left_striker.y_dim) and (self.position[0] >= left_striker.position[0] + left_striker.x_dim) and (next_position[0] <= left_striker.position[0] + left_striker.x_dim):
-            self.velocity[0] = -1*self.velocity[0]
-        if (right_striker.position[1] < self.position[1] < right_striker.position[1] + right_striker.y_dim) and (self.position[0] <= right_striker.position[0]) and (next_position[0] >= right_striker.position[0]):
-            self.velocity[0] = -1*self.velocity[0]
+            self.velocity[0] = -1*self.velocity[0] 
+            self.velocity[1] += left_striker.velocity
+        if (right_striker.position[1] < self.position[1] < right_striker.position[1] + right_striker.y_dim) and (self.position[0] <= right_striker.position[0] + right_striker.x_dim) and (next_position[0] >= right_striker.position[0]):
+            self.velocity[0] = -1*self.velocity[0] + right_striker.velocity[0]
+            self.velocity[1] += right_striker.velocity[1]*0.3
+
+        self.velocity = self.velocity*0.999
         return
 
     def move(self):
@@ -188,7 +207,7 @@ class Ball(Actor):
         return
     
     def draw(self):
-        self.plot.set_offsets(self.position_history.T)
+        self.plot.set_offsets(self.position_history[[1,0]].T)
         self.plot.set_alpha([0.2, 0.4, 0.6, 0.8, 1])
 
         pygame_pos = pygame.Vector2(self.position[0], self.position[1])
