0b3996c3d72489ce8f18aa705f70886484cd888b
MrTablez <loganvannevl17@gmail.com>
2024-02-29 00:10:30 -0800

    Additional Bug Fixes

--- camera/Green_Striker_Tracking.py
+++ camera/Green_Striker_Tracking.py
@@ -16,19 +16,32 @@ flag = 0
 # I was running into an issue where the countours object (which is an array of arrays I think) was 
 # initialized as empty on the first run through, or atleast the compiler believed it to be. So, the
 # purpose of the flag is to halt the cnt = contours[i] code until contours is correctly populated
-MIN_MATCH_COUNT = 10
+MIN_MATCH_COUNT = 6
+lower_green = np.array([40,60,20])
+upper_green = np.array([100,255,198])
 cap = cv.VideoCapture(2)
 while(1):
     # Standard setup for OpenCV video processing
     _, frame = cap.read()
     frame_Gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     height, width, _ = frame.shape
-    Extracted_Striker_Frame = cv.imread('ExtractedStriker.png')
+    # Threshold the HSV image to get only green colors, threshold values were received from the max 
+    # and min observed values from an online color picker, with a  sample image of the target object
+    mask = cv.inRange(frame_hsv, lower_green, upper_green)
+    #ret1, mask = cv.threshold(grayscale,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    blur = cv.medianBlur(mask,23)
+    # median blur to remove salt and pepper noise
+    blur2 = cv.blur(blur,(20,20))
+    # standard blur appears to be sufficient for our case. 20,20 was chosen experimentally
+    edges = cv.Canny(blur, 100, 200)
+    contours, _ = cv.findContours(blur2, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    Extracted_Striker_Frame = cv.imread('ThresholdStriker.png')
     Extracted_Striker_Frame_Gray = cv.cvtColor(Extracted_Striker_Frame, cv.COLOR_BGR2GRAY)
     # Initiate SIFT detector
     sift = cv.SIFT_create()
     # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(frame_Gray,None)
+    kp1, des1 = sift.detectAndCompute(blur2,None)
     kp2, des2 = sift.detectAndCompute(Extracted_Striker_Frame_Gray,None)
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
@@ -38,27 +51,35 @@ while(1):
     # store all the good matches as per Lowe's ratio test.
     good = []
     for m,n in matches:
-        if m.distance < 0.7*n.distance:
+        if m.distance < 0.9*n.distance:
             good.append(m)
     if len(good)>MIN_MATCH_COUNT:
-        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
-        matchesMask = mask.ravel().tolist()
-        h,w, _ = frame_Gray.shape
-        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv.perspectiveTransform(pts,M)
-        Extracted_Striker_Frame_Gray = cv.polylines(Extracted_Striker_Frame_Gray,[np.int32(dst)],True,255,3, cv.LINE_AA)
+        #src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        #dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+        #M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+        #matchesMask = mask.ravel().tolist()
+        #h,w = blur2.shape
+        #pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        #dst = cv.perspectiveTransform(pts,M)
+        #blur2 = cv.polylines(blur2,[np.int32(dst)],True,255,3, cv.LINE_AA)
+        print ("Good Threshold Found, Exiting")
+        exit()
     else:
         print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
         matchesMask = None
+        
     draw_params = dict(matchColor = (0,255,0), # draw matches in green color
                    singlePointColor = None,
                    matchesMask = matchesMask, # draw only inliers
                    flags = 2)
-    img3 = cv.drawMatches(frame_Gray,kp1,Extracted_Striker_Frame_Gray,kp2,good,None,**draw_params)
+    img3 = cv.drawMatches(blur2,kp1,Extracted_Striker_Frame_Gray,kp2,good,None,**draw_params)
     img3_small = cv.resize(img3, (0, 0), fx = 0.5, fy = 0.5)
     cv.imshow('matching',img3_small)
+    cv.imshow('frame',frame)
+    cv.imshow('Extractedstriker' , Extracted_Striker_Frame)
+    cv.imshow('mask',mask)
+    cv.imshow('blur',blur)
+    cv.imshow('blur2', blur2)
     #plt.imshow(img3, 'gray'),plt.show()
     """
     minHessian = 400
