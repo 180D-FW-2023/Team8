e508cade64bd988cc4e74b020e6d5dade9261f4f
Nabeel Shahid <nshahid@ucla.edu>
2024-03-04 21:59:43 -0800

    Add documentation

--- imu/imu_reading.py
+++ imu/imu_reading.py
@@ -2,20 +2,20 @@ import asyncio
 from ably import AblyRealtime
 import time
 from config import config
-import re
 
 def listener(message):
+    # Listener takes message data and stores it in shared queue
     data = parse_message(message.data)
     config.imu.put(data)
 
 def parse_message(data):
-
+    # Parser convets to x y z to float format
     split = str(data).split(': ', )
     split = (float(split[1][0:-6]), float(split[2][0:-6]), float(split[3][0:-9]))
     return split
 
 async def imu_sub():
-  
+    # Ably script to set up subscriber
     ably = AblyRealtime('zsW-PQ.nSlItw:zqlmeBRBlN7nYanF6LzG4ZlsgSn_3O9I-J0LVMgr7m0')
     await ably.connection.once_async('connected')
 
