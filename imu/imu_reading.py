34c7950c8fca7d85b057eced4304af37e4e1f217
Nabeel Shahid <nshahid@ucla.edu>
2024-03-04 20:49:51 -0800

    parse message data

--- imu/imu_reading.py
+++ imu/imu_reading.py
@@ -10,15 +10,14 @@ def listener(message):
 
 def parse_message(data):
 
-    values = re.findall(r'(accX:|accY:|accZ:)\s*([\d.-]+)', str(data))
-    final_values = [float(i[1]) for i in values]
-    return final_values
+    split = data.split(': ', )
+    split = (float(split[1][0:-6]), float(split[2][0:-6]), float(split[3][0:-9]))
+    return split
 
 async def imu_sub():
   
     ably = AblyRealtime('zsW-PQ.nSlItw:zqlmeBRBlN7nYanF6LzG4ZlsgSn_3O9I-J0LVMgr7m0')
     await ably.connection.once_async('connected')
-    print('Connected to Ably')
 
     channel = ably.channels.get('controller:1')
 
