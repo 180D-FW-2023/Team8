0ceff3fde8c91919354cc3c7ba737b08f7aa7c01
Rahul Chander <rahul@joinpoppin.com>
2024-03-06 16:14:26 -0800

    setup queue details + comments

--- config/config.py
+++ config/config.py
@@ -1,7 +1,13 @@
 import queue
 
+# Queues initialized for passing data between code running in different threads
+# Because the queues are essentially global
+
 shared = queue.Queue()
 camera = queue.Queue()
 imu = queue.Queue()
+
+# Global variables
+
 threshold = ((40, 60, 20), (100, 255, 198))
 state_signals = {'CAL_SIG' : 0, 'BEGIN_CAL_SIG': 0, 'GAME_SIG' : 0}
