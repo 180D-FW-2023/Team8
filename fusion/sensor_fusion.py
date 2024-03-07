9e3fdf4a0f7d5ac398b335c1afe45120fb270326
Rahul Chander <rahul@joinpoppin.com>
2024-03-06 16:10:43 -0800

    set up IMU sensor fusion

--- fusion/sensor_fusion.py
+++ fusion/sensor_fusion.py
@@ -1,17 +1,31 @@
 from config import config
 import time
 
+imu_weight = 0.03
 def run_fusion():
     camera_reading = 0
     while 1:
-        if not config.camera.empty():
+        if not config.imu.empty():
+            imu_reading = config.imu.get_nowait()
+            position_change = (imu_reading[0] * imu_weight, imu_reading[1] * imu_weight)
+            previous_reading = camera_reading
+            camera_reading = config.camera.get_nowait()
+            camera_reading = fuse_data(camera_reading, position_change)
+            if camera_reading != previous_reading:
+                config.shared.put(camera_reading)
+        elif not config.camera.empty():
             previous_reading = camera_reading
             camera_reading = config.camera.get_nowait()
             if camera_reading != previous_reading:
                 config.shared.put(camera_reading)
-        if not config.imu.empty():
-            imu_reading = config.imu.get_nowait()
+
         time.sleep(0.01)
             
-                
+# Acc simulates velocity
+def fuse_data(camera_position, position_change):
+    x, y = camera_position
+    deltaX, deltaY = position_change
+    new_x = x + deltaX
+    new_y = y + deltaY
+    return new_x, new_y
     
